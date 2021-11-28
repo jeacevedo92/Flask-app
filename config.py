272c59a08bc8cc4f3a74fca2a2e4ae953f2f9fdf
jhonacevedo <jhonacevedo@habi.co>
2021-11-28 17:20:22 -0500

    add envs database

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     #Database URL
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:fl4zk**@localhost:3310/flaskmysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://${{ secrets.USER_DATABASE }}:${{ secrets.PASS_DATABASE }}@${{ secrets.HOST_DATABASE }}:${{ secrets.PORT_DATABASE }}/flaskmysql'
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
